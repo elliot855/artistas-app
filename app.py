855ba4bea0ff320200c7cb83edd5e848d21cdd0e
elliot855 <difer@ucm.es>
2026-04-20 11:43:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,11 @@ model = cargar_modelo()
 # Transformaciones
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
 ])
 
 st.title("🎨 Clasificador de Obras de Arte")
