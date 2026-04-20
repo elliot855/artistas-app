3be1754ae1f6d1a33555b51ccd2d5e42c4ed1ca8
elliot855 <difer@ucm.es>
2026-04-20 13:06:34 +0200

    Update arquitectura.py

--- arquitectura.py
+++ arquitectura.py
@@ -1,7 +1,13 @@
-import timm
+import torch
 import torch.nn as nn
+import timm
 
-def crear_modelo(num_classes=4):
-    model = timm.create_model('efficientnet_b0', pretrained=False)
-    model.classifier = nn.Linear(model.classifier.in_features, num_classes)
-    return model
+class ModeloArtistas(nn.Module):
+    def __init__(self, num_classes=4):
+        super(ModeloArtistas, self).__init__()
+
+        self.model = timm.create_model('efficientnet_b0', pretrained=False)
+        self.model.classifier = nn.Linear(self.model.classifier.in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
